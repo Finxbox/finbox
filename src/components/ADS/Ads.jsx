4c67007282ad8748e46191830f255c0ecbc40186
Finxbox <info.finxbox@gmail.com>
2025-03-15 17:53:15 +0530

    Access to arun

---
+++ src/components/ADS/Ads.jsx
@@ -0,0 +1,26 @@
+function Ads() {
+  return (
+    <div>
+      <div className="Ads">
+        <a href="https://amzn.to/3Ec050s" target="_blank">
+          <img
+            src="/prime music.jpg"
+            alt="Amazon seller banner"
+            className="mx-auto"
+          />
+        </a>
+
+        <div className="flex justify-center">
+          {" "}
+          <p className="text-center text-NavPurple w-6/12  pt-10 text-md font-semibold">
+            Some links on this page are Amazon affiliate links. If you make a
+            purchase through them, I may earn a small commission at no extra
+            cost to you. Thank you for supporting, Happy shopping!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Ads;
--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,3 +1,5 @@
+import Ads from "../components/ADS/Ads";
+
 const Store = () => {
   const searchResult = {
     Items: [
@@ -162,23 +164,8 @@ const Store = () => {
 
   return (
     <div>
-      <div className="Ads">
-        <a href="https://amzn.to/3Ec050s" target="_blank">
-          <img
-            src="/prime music.jpg"
-            alt="Amazon seller banner"
-            className="mx-auto"
-          />
-        </a>
-
-        <div className="flex justify-center">
-          {" "}
-          <p className="text-center text-NavPurple w-6/12  pt-10 text-md font-semibold">
-            Some links on this page are Amazon affiliate links. If you make a
-            purchase through them, I may earn a small commission at no extra
-            cost to you. Thank you for supporting, Happy shopping!
-          </p>
-        </div>
+      <div>
+        <Ads />
       </div>
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 p-10">
         {searchResult.Items.map((item) => (
